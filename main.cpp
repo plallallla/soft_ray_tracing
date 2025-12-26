54982ab9b2b61b4be478ce5eed57e76cf918f039
pnghfng <pnghfng52388@icloud.com>
2025-12-26 17:33:58 +0800

    update

--- main.cpp
+++ main.cpp
@@ -12,27 +12,25 @@ int main(int, char**)
 {
     HitTableList world;
     
-
-    Lambertian ground_mat{ {.8f, .8f, .0f} };
-    Lambertian l{ {.1f, .2f, .5f} };
-    Metal m{ { .8f, .6f, .2f }, 0.2 };
-    Dielectric d{ 1.5f };
-
+    auto ground_mat = std::make_shared<Lambertian>(glm::vec3{.8f, .8f, .0f});
+    auto l = std::make_shared<Lambertian>(glm::vec3{.1f, .2f, .5f});
+    auto m = std::make_shared<Metal>(glm::vec3{ .8f, .6f, .2f }, 0.2);
+    auto d = std::make_shared<Dielectric>(1.5f);
 
     Sphere ground{glm::vec3{0.f, 100.5f, -1.5f}, 100.f};
-    ground._material = std::make_shared<Lambertian>(ground_mat);
+    ground._material = ground_mat;
     world.add(std::make_shared<Sphere>(ground));
 
     Sphere mid{glm::vec3{0.f, 0.f, -1.5f}, .5f};
-    mid._material = std::make_shared<Lambertian>(l);    
+    mid._material = l;
     world.add(std::make_shared<Sphere>(mid));
 
     Sphere left{glm::vec3{-1.f, 0.f, -1.5f}, .5f};
-    left._material = std::make_shared<Dielectric>(d);
+    left._material = d;
     world.add(std::make_shared<Sphere>(left));
 
     Sphere right{glm::vec3{1.f, 0.f, -1.5f}, .5f};
-    right._material = std::make_shared<Metal>(m);
+    right._material = m;
     world.add(std::make_shared<Sphere>(right));
 
     Camera camera;
