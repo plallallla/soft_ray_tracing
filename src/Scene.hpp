75578d0c1757c1a083995dfdbad3ec24fcc02f8a
pnghfng <pnghfng52388@icloud.com>
2026-01-01 01:12:45 +0800

    初步搭建cornell_box场景

---
+++ src/Scene.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "HitTable.hpp"
+#include "Material.hpp"
+#include "Transform.hpp"
+
+inline HitTableList cornell_box()
+{
+    HitTableList world;
+    auto red = std::make_shared<Lambertian>(glm::vec3(.65f, .05f, .05f));
+    auto white = std::make_shared<Lambertian>(glm::vec3(.73f, .73f, .73f));
+    auto green = std::make_shared<Lambertian>(glm::vec3(.12f, .45f, .15f));
+    auto light = std::make_shared<DiffuseLight>(glm::vec3(15.f, 15.f, 15.f));
+
+    auto depth = -6.f;
+    auto length = -7.f;
+    auto half_length = 3.5f;
+
+
+    world.add(std::make_shared<Quad>(
+        glm::vec3(half_length, half_length, -6.f), glm::vec3(length, 0.f, 0.f), glm::vec3(0.f, length, 0.f), white));
+    world.add(std::make_shared<Quad>(
+        glm::vec3(half_length, half_length, -6.f), glm::vec3(length, 0.f, 0.f), glm::vec3(0.f, 0.f, -length), white));  
+    world.add(std::make_shared<Quad>(
+        glm::vec3(-half_length, -half_length, -6.f), glm::vec3(-length, 0.f, 0.f), glm::vec3(0.f, 0.f, -length), white));                
+    world.add(std::make_shared<Quad>(
+        glm::vec3(half_length, half_length, -6.f), glm::vec3(0.f, length, 0.f), glm::vec3(0.f, 0.f, -length), red));         
+    world.add(std::make_shared<Quad>(
+        glm::vec3(-half_length, -half_length, -6.f), glm::vec3(0.f, -length, 0.f), glm::vec3(0.f, 0.f, -length), green));         
+
+
+    // shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), white);
+    // box1 = make_shared<rotate_y>(box1, 15);
+    // box1 = make_shared<translate>(box1, vec3(265,0,295));
+    // world.add(box1);
+
+    // shared_ptr<hittable> box2 = box(point3(0,0,0), point3(165,165,165), white);
+    // box2 = make_shared<rotate_y>(box2, -18);
+    // box2 = make_shared<translate>(box2, vec3(130,0,65));
+    // world.add(box2);
+
+    return world;
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,72 +2,18 @@
 #include <glm/detail/qualifier.hpp>
 #include <glm/fwd.hpp>
 #include <glm/geometric.hpp>
-#include <memory>
 
-#include "HitTable.hpp"
-#include "Material.hpp"
-#include "Transform.hpp"
 #include "tgaimage.hpp"
 #include "Camera.hpp"
-
-
-void scene()
-{
-    HitTableList world;
-    
-}
+#include "Scene.hpp"
 
 int main(int, char**)
 {
-    HitTableList world;
-    
-    auto ground_mat = std::make_shared<Lambertian>(glm::vec3{.5f, .5f, .0f});
-
-    auto light = std::make_shared<DiffuseLight>(glm::vec3(1.f, 1.f, 0.f));
-
-
-
-    auto l = std::make_shared<Lambertian>(glm::vec3{.1f, .2f, .5f});
-    auto m = std::make_shared<Metal>(glm::vec3{ .8f, .6f, .2f }, 0.2);
-    auto d = std::make_shared<Dielectric>(1.5f);
-
-    Sphere ground{glm::vec3{0.f, 100.5f, -1.5f}, 100.f};
-    ground._material = ground_mat;
-    // world.add(std::make_shared<Sphere>(ground));
-
-
-    // auto unit_quad = std::make_shared<Quad>(glm::vec3(0.f, 0.f, -4.f), glm::vec3(1.f, 0.f, 0.f), glm::vec3(0.f, 2.f, 0.f));
-    // unit_quad->_material = ground_mat;
-
-    HitTablePtr box = CreateBox(1.f, 1.f, 1.f, ground_mat);
-    box = apply_transform<RotateY>(box, 45.f);
-    box = apply_transform<Translate>(box, glm::vec3{0.f, 0.f, -4.f});
-    world.add(box);
-    // world.add(std::make_shared<Translate>(q, glm::vec3{1.f,1.f,0.f}));
-    // world.add(std::make_shared<Translate>(q, glm::vec3{-1.f,-1.f,0.f}));
-    // world.add(std::make_shared<RotateY>(q, 45.0f));
-
-    // Sphere mid{glm::vec3{0.f, 0.f, -1.5f}, .5f};
-    // mid._material = l;
-    // world.add(std::make_shared<Sphere>(mid));
-
-    // Sphere left{glm::vec3{-1.f, 0.f, -1.5f}, .5f};
-    // left._material = d;
-    // world.add(std::make_shared<Sphere>(left));
-
-    // Sphere right{glm::vec3{1.f, 0.f, -1.5f}, .5f};
-    // right._material = m;
-    // world.add(std::make_shared<Sphere>(right));
-
+    HitTableList world = cornell_box();
     Camera camera;
     TGAImage framebuffer(camera.get_image_width(), camera.get_image_height(), TGAImage::RGB);
     camera.render(framebuffer, world);
     framebuffer.write_tga_file("ray_trace.tga");
-
     system("open ray_trace.tga");    
-
-
-
-
 }
 
